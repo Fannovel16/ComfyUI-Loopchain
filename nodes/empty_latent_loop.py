e19fa6b4102aca7f4fd4f2edb22a3c665a791053
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-08-23 18:38:49 +0700

    init

--- nodes/empty_latent_loop.py
+++ nodes/empty_latent_loop.py
@@ -10,9 +10,10 @@ class EmptyLatentImageLoop:
     def INPUT_TYPES(s):
         return {"required": { "width": ("INT", {"default": 512, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
                               "height": ("INT", {"default": 512, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
-                              "batch_size": ("INT", {"default": 1, "min": 1, "max": 64})},
+                              "batch_size": ("INT", {"default": 1, "min": 1, "max": 64}),
                               "num_loop": ("INT", {"default": 1, "min": 0}),
                               "loop_idx": ("INT", {"default": 0, "min": 0})
+                              }, 
                 }
     RETURN_TYPES = ("LATENT", "INT",)
     OUTPUT_NAMES = ("LATENT", "INDEX")
