c1b00fad8c1dd4c5c9c55d4daed180d5720f6731
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-08-25 22:45:46 +0700

    Nonsense timeout?

--- web/loopchain.js
+++ web/loopchain.js
@@ -38,6 +38,7 @@ class LoopchainPipeline {
                     if (!output.links?.[0]) continue;
                     const loopNode = getNodeByLink(output.links[0], "output");
                     await findWidgetByName(loopNode, "Queue").callback();
+                    await new Promise(re => setTimeout(re, 1000)); 
                 }
             })();
         });
